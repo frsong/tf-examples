415723c6e87ed8ef3b6eb389ce931e7493fbea2c
frsong <song.francis@gmail.com>
2017-04-29 10:59:19 -0400

    Initial commit

--- char_rnn_model.py
+++ char_rnn_model.py
@@ -50,6 +50,7 @@ class Model(object):
         # len(initial_state) = num_layers
         # state[i].c.shape   = [batch_size, rnn_size]
         self.initial_state = self.cell.zero_state(batch_size, tf.float32)
+
         outputs, self.final_state = tf.nn.dynamic_rnn(
             self.cell, inputs, initial_state=self.initial_state
             )
